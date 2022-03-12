3835d941341cbfb36b7e7118e513bcc11668402a
Mwibutsa Floribert <mflohost@gmail.com>
2022-03-12 18:23:46 +0200

    Project configuration

---
+++ babel.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  presets: ["@babel/preset-env"],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        alias: {
+          "@helpers": "./src/helpers",
+          "@testUtils": "./src/testUtils",
+          "@middleware": "./src/middleware",
+          "@models": "./src/database/models",
+          "@controllers": "./src/controllers",
+          "@routes": "./src/routes",
+          "@dbConfig": "./src/database/dbConfig",
+          "@constants": "./src/constants",
+          "@database": "./src/database",
+        },
+      },
+    ],
+  ],
+};
